27218ccaa4d9603c89a11f14b438e2c8bab1e626
Yash Goyal <yashgo0018@gmail.com>
2024-06-07 09:32:26 +0000

    using bigger key

--- packages/hardhat/constants.ts
+++ packages/hardhat/constants.ts
@@ -4,14 +4,20 @@ export const VerifierContractName = "Verifier";
 export const TopupCreditContractName = "TopupCredit";
 
 // zk registry config
-export const stateTreeDepth = 10;
-export const intStateTreeDepth = 1;
-export const messageTreeDepth = 2;
-export const voteOptionTreeDepth = 2;
-export const messageBatchDepth = 1;
+// export const stateTreeDepth = 10;
+// export const intStateTreeDepth = 1;
+// export const messageTreeDepth = 2;
+// export const voteOptionTreeDepth = 2;
+// export const messageBatchDepth = 1;
 
-export const processMessagesZkeyPath = "./zkeys/ProcessMessages_10-2-1-2_test/ProcessMessages_10-2-1-2_test.0.zkey";
+export const messageTreeDepth = 7;
+export const stateTreeDepth = 14;
+export const voteOptionTreeDepth = 3;
+export const messageBatchDepth = 2;
+export const intStateTreeDepth = 5;
+
+export const processMessagesZkeyPath = "./zkeys/ProcessMessages_14-7-2-3_test/processmessages_14-7-2-3_test.zkey";
 export const processMessagesNonQvZkeyPath =
-  "./zkeys/ProcessMessagesNonQv_10-2-1-2_test/ProcessMessagesNonQv_10-2-1-2_test.0.zkey";
-export const tallyVotesZkeyPath = "./zkeys/TallyVotes_10-1-2_test/TallyVotes_10-1-2_test.0.zkey";
-export const tallyVotesNonQvZkeyPath = "./zkeys/TallyVotesNonQv_10-1-2_test/TallyVotesNonQv_10-1-2_test.0.zkey";
+  "./zkeys/ProcessMessagesNonQv_14-7-2-3_test/processmessagesnonqv_14-7-2-3_test.zkey";
+export const tallyVotesZkeyPath = "./zkeys/TallyVotes_14-5-3_test/tallyvotes_14-5-3_test.zkey";
+export const tallyVotesNonQvZkeyPath = "./zkeys/TallyVotesNonQv_14-5-3_test/tallyvotesnonqv_14-5-3_test.zkey";
